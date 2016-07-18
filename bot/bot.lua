65b2a5d6895a985cb9fcdf77d2abdffe47d61e62
BooM-amour <lov3_4_3v3r_666@yahoo.com>
2016-07-18 16:13:48 -0700

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -230,7 +230,6 @@ function create_config( )
     enabled_plugins = {
      "ctrl",
      "plugins",
-     "poker",
      "version",
      "addplug",
      "help",
@@ -241,7 +240,7 @@ function create_config( )
      "weather",
      "webshot",
      },
-    sudo_users = {223216007},
+    sudo_users = {221176886},
     admin_users = {},
     disabled_channels = {}
   }
